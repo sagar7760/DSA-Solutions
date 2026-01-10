ef2dc93213a8c2bd90f2abf8b215e7acc461d5f1
Sagar Soradi <sagarsordi011@gmail.com>
2026-01-10 19:14:08 +0530

    fixed the code with correct logic for insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -9,16 +9,15 @@ public class InsertionSort {
         }
     }
 
-    static int[] insertionSort(int[] nums){
+    static void insertionSort(int[] nums){
         for(int i=0;i<nums.length;i++){
-            int j=i;
-            while (j>0 && nums[j-1]>nums[j]) {
-                int temp=nums[j];
-                nums[j]=nums[j-1];
-                nums[j-1]=temp;
-                j--;
+            int key=nums[i];
+            int j=i-1;
+                while (j>=0 && nums[j]>key){
+                    nums[j+1]=nums[j];
+                    j--;
+                }
+                nums[j+1]=key;
             }
         }
-        return nums;
     }
-}
